6bd0b7feb2ba6cd6eb54b102bdd099ef1c15eb8d
Vlad Borovtsov <borovcov.vlad@gmail.com>
2020-11-20 16:59:21 +0700

    add code to manage hardware torch

--- the Light/ViewController.swift
+++ the Light/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import AVKit
 
 class ViewController: UIViewController {
 
@@ -25,7 +26,32 @@ class ViewController: UIViewController {
 
   fileprivate func updateUI() {
     self.view.backgroundColor = colors[mode];
+    toggleTorch(mode != colors.count-1) //torch should be on for any colors except black.
   }
+  
+  fileprivate func toggleTorch(_ on: Bool) {
+    guard let device = AVCaptureDevice.default(for: AVMediaType.video) else { return }
+    guard device.hasTorch else { return }
+    
+    do {
+      try device.lockForConfiguration()
+      
+      if on {
+        do {
+          try device.setTorchModeOn(level: 1.0)
+        } catch {
+          print(error)
+        }
+      }
+      else {
+        device.torchMode = AVCaptureDevice.TorchMode.off
+      }
+      device.unlockForConfiguration()
+    } catch {
+      print(error)
+    }
+  }
+  
   
   override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
     mode += 1;
